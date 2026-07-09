7d887b4b62d3475e05f8d5fe14abd50eb474aa6e
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-07-09 07:36:10 +0000

    comment partial

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,26 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = Current.user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = Current.user.book_comments.new(book_comment_params)
+    @book_comment.book_id = book.id
+    @book_comment.save
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to book_path(@book) }
+    end
   end
 
   def destroy
-    book_comment = BookComment.find(params[:id])
-    book_comment.destroy
-    redirect_back fallback_location: book_path(book_comment.book)
+    @book_comment = BookComment.find(params[:id])
+    @book = @book_comment.book
+    @book_comment.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: book_path(@book) }
+    end
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,25 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = Current.user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = Current.user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back fallback_location: books_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: books_path }
+    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = Current.user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = Current.user.favorites.find_by(book_id: @book.id)
     favorite&.destroy
-    redirect_back fallback_location: books_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: books_path }
+    end
   end
 
 end
